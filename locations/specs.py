e746aa9d279ee6bb3c11752f2e4d6e4f8aa380aa
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-03-23 16:26:36 +0300

    fixes

--- locations/specs.py
+++ locations/specs.py
@@ -7,6 +7,12 @@ class ResizeThumb( processors.Resize ):
     height = 98
     crop = True
 
+class LogoThumb( processors.Resize ):
+    width = 188
+    height = 138
+    crop = True
+
+
 # now let's create an adjustment processor to enhance the image at small sizes
 class EnchanceThumb( processors.Adjustment ):
     contrast = 1.2
@@ -17,3 +23,10 @@ class Thumbnail( ImageSpec ):
     access_as = 'thumbnail'
     pre_cache = False
     processors = [ResizeThumb, EnchanceThumb]
+
+
+# now we can define our thumbnail spec
+class Logo( ImageSpec ):
+    access_as = 'logo'
+    pre_cache = False
+    processors = [LogoThumb, EnchanceThumb]
