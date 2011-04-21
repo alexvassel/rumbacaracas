1bc142e5fe29d73ded715402e206e03439592021
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-04-21 13:19:13 +0400

    fix paging

--- locations/templatetags/tools.py
+++ locations/templatetags/tools.py
@@ -60,37 +60,6 @@ def locations_paginator( context, adjacent_pages = 2 ):
 register.inclusion_tag( 'paginator.html', takes_context = True )( locations_paginator )
 
 
-
-def common_paginator( context, adjacent_pages = 2 ):
-
-    current_page = context['current_page']
-    current_paginator = context['current_paginator']
-    startPage = max( current_page.number - adjacent_pages, 1 )
-    if startPage <= 3: startPage = 1
-    endPage = current_page.number + adjacent_pages + 1
-    if endPage >= current_paginator.num_pages - 1: endPage = current_paginator.num_pages + 1
-    page_numbers = [n for n in range( startPage, endPage ) \
-            if n > 0 and n <= current_paginator.num_pages]
-    page_obj = current_paginator.page
-    paginator = current_paginator
-    return {
-        'page_obj': page_obj,
-        'paginator': paginator,
-        'page': current_page.number,
-        'pages': current_paginator.num_pages ,
-        'page_numbers': page_numbers,
-        'next': current_page.next_page_number(),
-        'previous': current_page.previous_page_number() ,
-        'has_next': current_page.has_next(),
-        'has_previous': current_page.has_previous(),
-        'show_first': 1 not in page_numbers,
-        'show_last': current_paginator.num_pages  not in page_numbers,
-    }
-
-register.inclusion_tag( 'common_paginator.html', takes_context = True )( locations_paginator )
-
-
-
 @register.filter
 def hash( h, key ):
     try:
