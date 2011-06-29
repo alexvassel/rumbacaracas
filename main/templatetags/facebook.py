0229d1af8477be11826e1f6ecdc1be4c123dfec0
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-06-29 12:34:37 +0400

    social integration

--- main/templatetags/facebook.py
+++ main/templatetags/facebook.py
@@ -19,15 +19,30 @@ def facebook_comments( url ):
 
 
 @register.inclusion_tag( 'main/facebook_like.html' )
-def facebook_like( url, width = 150 ):
+def facebook_like( url, width = 90 , send = True ):
     full_url = settings.FULL_BASE_URL + url
-    return dict( url = full_url, width = width )
+    return dict( url = full_url, width = width, send = send )
+
+
+
+@register.inclusion_tag( 'main/facebook_send.html' )
+def facebook_send( url ):
+    full_url = settings.FULL_BASE_URL + url
+    return dict( url = full_url )
+
 
 
 @register.inclusion_tag( 'main/facebook_comments_count.html' )
 def facebook_comments_count( url ):
     full_url = settings.FULL_BASE_URL + url
     return dict( url = full_url )
+
+
+
+@register.inclusion_tag( 'main/facebook_group.html' )
+def facebook_group():
+    return dict( url = settings.FULL_BASE_URL )
+
 
 
 
---
+++ main/templatetags/twitter.py
@@ -0,0 +1,17 @@
+from django import template
+from django.conf import settings
+from socialregistration.utils import _https
+
+register = template.Library()
+
+@register.inclusion_tag( 'main/twitter_js.html' )
+def twitter_js_tag():
+    return {
+            'is_https' : bool( _https() )
+    }
+
+
+@register.inclusion_tag( 'main/twitter_share.html' )
+def twitter_share( url, text ):
+    full_url = settings.FULL_BASE_URL + url
+    return dict( url = full_url, text = text )
