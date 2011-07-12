4a7bc14613cd8ac6734ad1cff44561949615f9fe
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-07-12 14:40:07 +0400

    magazine

--- magazine/models.py
+++ magazine/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from preferences.models import Preferences
 from django.utils.translation import ugettext_lazy as _
+from tinymce import models as tinymce_models
 
 class LastMagazinePreferences( Preferences ):
     __module__ = 'preferences.models'
-    anotation = models.TextField(_('Last Magazine Anotation'))
+    anotation = tinymce_models.HTMLField(_('Last Magazine Anotation'))
 
     class Meta():
         verbose_name = "Last Magazine Anotation"
