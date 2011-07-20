deec81a53c56f01ee5b5e77711e9128ba0db88cb
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-07-20 17:37:35 +0400

    fixes

--- host_settings/linexternal_td.py
+++ host_settings/linexternal_td.py
@@ -35,7 +35,7 @@ DATABASES = {
     },
     'olddata': {
             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'oldbogota', # Or path to database file if using sqlite3.
+            'NAME': 'oldbogotadata', # Or path to database file if using sqlite3.
             'USER': 'root', # Not used with sqlite3.
             'HOST': 'linexternal.td', # Set to empty string for localhost. Not used with sqlite3.
             'PASSWORD': 'cleopatra', # Not used with sqlite3.
