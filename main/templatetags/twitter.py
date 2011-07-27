b78da13fefafc9dcf12d6c4e6fb7ec0610b6848e
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-07-27 17:07:34 +0400

    fixes

--- main/templatetags/twitter.py
+++ main/templatetags/twitter.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.utils.html import strip_tags
 from socialregistration.utils import _https
 
 register = template.Library()
@@ -14,4 +15,4 @@ def twitter_js_tag():
 @register.inclusion_tag( 'main/twitter_share.html' )
 def twitter_share( url, text ):
     full_url = settings.FULL_BASE_URL + url
-    return dict( url = full_url, text = text )
+    return dict( url = full_url, text = strip_tags(text) )
