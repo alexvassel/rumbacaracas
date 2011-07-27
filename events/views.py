fbfc1eaa692da27d845c7215f1b0efc4fcaca0bf
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-07-27 17:54:03 +0400

    fixes

--- events/views.py
+++ events/views.py
@@ -15,21 +15,7 @@ from django.template.defaultfilters import slugify
 from django.http import HttpResponseRedirect
 from django.forms import forms
 
-
-months = {
-        1 : "January",
-        2 : "February",
-        3 : "March",
-        4 : "April",
-        5 : "May",
-        6 : "June",
-        7 : "July",
-        8 : "August",
-        9 : "September",
-        10 : "October",
-        11 : "November",
-        12 : "December",
-}
+from django.utils.dates import MONTHS as months
 
 calendar.setfirstweekday( 6 )
 #MO, TU, WE, TH, FR, SA, SU
