794a9dea449bf3621fe9b4280c138bfd1089a667
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-09-12 19:53:54 +0400

    fixes caracas server config

--- host_settings/linexternal_td.py
+++ host_settings/linexternal_td.py
@@ -1,22 +1,22 @@
 TEMPLATE_DIRS = ( 
-    '/srv/vhosts/rumbabogota/templates',
+    '/srv/vhosts/rumbacaracas/templates',
  )
 
 FIXTURE_DIRS = ( 
-   '/srv/vhosts/rumbabogota/locations/fixtures/',
+   '/srv/vhosts/rumbacaracas/locations/fixtures/',
  )
 
 
 OLDBOGOTA_PHOTO_PATH = '/srv/vhosts/oldbogota/processed'
 
-FULL_BASE_URL = 'http://213.33.244.154:88'
+FULL_BASE_URL = 'http://213.33.244.154:89'
 
-MEDIA_ROOT = '/srv/vhosts/rumbabogota/media/'
+MEDIA_ROOT = '/srv/vhosts/rumbacaracas/media/'
 
-STATIC_DOC_ROOT = '/srv/vhosts/rumbabogota/media/'
+STATIC_DOC_ROOT = '/srv/vhosts/rumbacaracas/media/'
 
 LOCALE_PATHS = ( 
-    '/srv/vhosts/rumbabogota/locale',
+    '/srv/vhosts/rumbacaracas/locale',
  )
 
 OLDDATABOGOTA_PHOTO_PATH = '/mnt/maksim/Documents/Develop/rumbabogota/olddata/'
@@ -27,7 +27,7 @@ FAKE_IMPORT_IMAGE = '/mnt/maksim/test_images/tst_imgs/1.jpg'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bogota', # Or path to database file if using sqlite3.
+        'NAME': 'caracas', # Or path to database file if using sqlite3.
         'USER': 'root', # Not used with sqlite3.
         'PASSWORD': 'cleopatra', # Not used with sqlite3.
         'HOST': 'linexternal.td', # Set to empty string for localhost. Not used with sqlite3.
