2a9a3e14398c47b9b0281a82e24bedc290a8ce53
Nikolay <dagger@localhost.localdomain>
2011-09-12 19:07:49 +0400

    Add local settings file

--- host_settings/localhost_localdomain.py
+++ host_settings/localhost_localdomain.py
@@ -1,25 +1,25 @@
 FIXTURE_DIRS = ( 
-   '/www/rumba/rumbabogota/locations/fixtures/',
+   '/www/rumba/rumbacaracas/locations/fixtures/',
  )
 
 TEMPLATE_DIRS = ( 
-    '/www/rumba/rumbabogota/templates',
+    '/www/rumba/rumbacaracas/templates',
  )
 
 OLDBOGOTA_PHOTO_PATH = '/home/maksim/Documents/Develop/oldbogota/processed'
 
-OLDDATABOGOTA_PHOTO_PATH = '/www/rumba/rumbabogota/olddata/'
+OLDDATABOGOTA_PHOTO_PATH = '/www/rumba/rumbacaracas/olddata/'
 FAKE_IMPORT_IMAGE = '/home/maksim/test_images/tst_imgs/1.jpg'
 
 
-FULL_BASE_URL = 'http://localhost.td'
+FULL_BASE_URL = 'http://localhost:8000/'
 
-MEDIA_ROOT = '/www/rumba/rumbabogota/media'
+MEDIA_ROOT = '/www/rumba/rumbacaracas/media'
 
-STATIC_DOC_ROOT = '/www/rumba/rumbabogota/media'
+STATIC_DOC_ROOT = '/www/rumba/rumbacaracas/media'
 
 LOCALE_PATHS = ( 
-    '/www/rumba/rumbabogota/locale/',
+    '/www/rumba/rumbacaracas/locale/',
  )
 
 
@@ -27,7 +27,7 @@ LOCALE_PATHS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bogota', # Or path to database file if using sqlite3.
+        'NAME': 'caracas', # Or path to database file if using sqlite3.
         'USER': 'root', # Not used with sqlite3.
         'PASSWORD': 'cydemAjD', # Not used with sqlite3.
         'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
@@ -51,9 +51,9 @@ CACHES = {
 }
 
 
-FACEBOOK_APP_ID = '180051822005393'
+FACEBOOK_APP_ID = '237180929639489'
 FACEBOOK_API_KEY = FACEBOOK_APP_ID #'15d12186d338568b8b5634e27aafb7cd'
-FACEBOOK_SECRET_KEY = 'aa512e0baa4dc6609320c8ce53b74dd1'
+FACEBOOK_SECRET_KEY = '68e652ad4024ba1c2a563e7ff833f856'
 
 
 TEMPLATE_LOADERS = (
