cc6e22d3431963c6aabe6538ecec9f145123b228
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-09-14 15:26:34 +0400

    import caracas

--- host_settings/linexternal_td.py
+++ host_settings/linexternal_td.py
@@ -35,7 +35,7 @@ DATABASES = {
     },
     'olddata': {
             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'oldbogotadata', # Or path to database file if using sqlite3.
+            'NAME': 'oldcaracas', # Or path to database file if using sqlite3.
             'USER': 'root', # Not used with sqlite3.
             'HOST': 'linexternal.td', # Set to empty string for localhost. Not used with sqlite3.
             'PASSWORD': 'cleopatra', # Not used with sqlite3.
