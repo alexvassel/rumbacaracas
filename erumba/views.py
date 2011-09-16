cde448b66cb5418af98160dac9657ac6cea23cb3
Nikolay <dagger@localhost.localdomain>
2011-09-16 19:48:54 +0400

    Ticket #2 Created main page for erumba

--- erumba/views.py
+++ erumba/views.py
@@ -1,5 +1,14 @@
 from decorators import render_to
+import datetime
+import dateutil.parser
+
 
 @render_to( "erumba/index.html" )
 def index(request):
-    return {}
+
+    from_date = datetime.date.today()
+    to_date = datetime.date.today() + datetime.timedelta(6)
+    return {
+        'from_date' : from_date,
+        'to_date' : to_date,
+    }
