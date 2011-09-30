8d57adaed79deec68783929ff28f82f22fe4021a
Maksim Nagaev <nagaev.maksim@gmail.com>
2011-09-30 15:50:17 +0400

    fix location category links

--- locations/urls.py
+++ locations/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns( 'locations.views',
     url( r'^por-zona$', 'area', name='location_by_area' ),
     url( r'^por-musica$', 'music', name='location_by_music' ),
     url( r'^por-comida$', 'food', name='location_by_food' ),
-    url( r'^tipo/(?P<group>\w+)$', "category_details" , name = "location_category" ),
+    url( r'^tipo/(?P<group>[-\w]+)$', "category_details" , name = "location_category" ),
     url( r'^(?P<slug>[-\w]+)/$', "detail", name = "location_details" ),
  )
