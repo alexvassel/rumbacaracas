35c890a2292f750ce9b45d342d95fc6fbfcdd9b0
Nikolay <dagger@localhost.localdomain>
2011-10-03 18:38:49 +0400

    Set default year of albums to item year

--- magazine/views.py
+++ magazine/views.py
@@ -4,6 +4,8 @@ from django.utils.safestring import mark_safe
 from django.conf import settings
 import hashlib
 import urllib
+from datetime import date
+import re
 
 # Find a JSON parser
 try:
@@ -71,12 +73,32 @@ def _get_issuu_last_document():
 @render_to( 'magazine/magazine.html' )
 def index( request ):
 
+    item_year = date.today().year
+
     folders_list = _get_issuu_folders_list()
-    albums    = filter(lambda a: a['folder']['itemCount'] > 0 and "notebook" not in a['folder']['name'], folders_list)
-    notebooks = filter(lambda a: a['folder']['itemCount'] > 0 and "notebook"     in a['folder']['name'], folders_list)
+    albums    = filter(lambda a: a['folder']['itemCount'] > 0 and re.match('^\d{4}$', a['folder']['name']), folders_list)
+
+    default_album = None
+    for album in albums:
+        if item_year == int(album['folder']['name']):
+            default_album = album
+
+    default_notebook = None
+    notebooks = filter(lambda a: a['folder']['itemCount'] > 0 and re.match('^Notebook \d{4}$', a['folder']['name']), folders_list)
+    for notebook in notebooks:
+        name = notebook['folder']['name'].replace('Notebook ')
+        if item_year == int(name):
+            default_notebook = notebook
 
     last_magazine_anotate = preferences.LastMagazinePreferences.anotation
     last_magazine_issuu_code = preferences.LastMagazinePreferences.issuu_code
 
-    return {'last_magazine_anotate': mark_safe(last_magazine_anotate), 'albums': albums, 'notebooks': notebooks, 'last_magazine_issuu_code' : last_magazine_issuu_code}
+    return {
+        'last_magazine_anotate': mark_safe(last_magazine_anotate),
+        'albums': albums,
+        'notebooks': notebooks,
+        'last_magazine_issuu_code' : last_magazine_issuu_code,
+        'default_notebook': default_notebook,
+        'default_album': default_album
+    }
 
