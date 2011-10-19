5acd82afb7ed164ce09960406ea93547e396dd88
Maksim <maksim@maksim.td>
2011-10-19 14:30:18 +0400

    tmp background configuration

--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.db import models
+from django.db.models import ImageField
+from preferences.models import Preferences
 from django.utils.translation import ugettext_lazy as _
 
+class MainBackgroundPreferences( Preferences ):
+    __module__ = 'preferences.models'
+    background_image = ImageField ( _( 'Main background image' ),upload_to = 'images/main_background')
+
+    class Meta():
+        verbose_name = "Main background image"
+        verbose_name_plural = "Main background images"
