9ce7406cdb6c32a2b6ada2ee3af9896706378ac6
Maksim <maksim@maksim.td>
2011-10-19 16:43:15 +0400

    disable tmp pref

--- main/models.py
+++ main/models.py
@@ -2,10 +2,11 @@ from django.db.models import ImageField
 from preferences.models import Preferences
 from django.utils.translation import ugettext_lazy as _
 
-class MainBackgroundPreferences( Preferences ):
-    __module__ = 'preferences.models'
-    background_image = ImageField ( _( 'Main background image' ),upload_to = 'images/main_background')
 
-    class Meta():
-        verbose_name = "Main background image"
-        verbose_name_plural = "Main background images"
+#class MainBackgroundPreferences( Preferences ):
+#    __module__ = 'preferences.models'
+#    background_image = ImageField ( _( 'Main background image' ),upload_to = 'images/main_background')
+#
+#    class Meta():
+#        verbose_name = "Main background image"
+#        verbose_name_plural = "Main background images"
