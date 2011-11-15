01bd5100f51467664fdd56cc3b05fff94c95949e
Maksim <maksim@maksim.td>
2011-11-15 21:53:25 +0400

    Admin: Notify when an event, tus fotos, video, sitio/locales or news are sent by users.

--- dashboard.py
+++ dashboard.py
@@ -5,12 +5,71 @@ contains the class for the main dashboard.
 To activate your index dashboard add the following to your settings.py::
     GRAPPELLI_INDEX_DASHBOARD = 'rumbacaracas.dashboard.CustomIndexDashboard'
 """
+from django.template.defaultfilters import capfirst
 
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 
 from grappelli.dashboard import modules, Dashboard
+from grappelli.dashboard.modules import DashboardModule
 from grappelli.dashboard.utils import get_admin_site_name
+from zinnia.models import Entry
+from events.models import Event
+from locations.models import Location
+from yourphotos.models import Photo
+from yourvideos.models import Video
+
+
+class NewContent(DashboardModule):
+    """
+    Module that lists not proccessed content.
+    """
+    title = _('Pending content')
+    template = 'main/admin_pending_content.html'
+    limit = 10
+    include_list = None
+    exclude_list = None
+
+    def __init__(self, title=None, limit=10, include_list=None,
+                 exclude_list=None, **kwargs):
+        self.include_list = include_list or []
+        self.exclude_list = exclude_list or []
+        kwargs.update({'limit': limit})
+        super(NewContent, self).__init__(title, **kwargs)
+
+    def init_with_context(self, context):
+        if self._initialized:
+            return
+        from django.db.models import Q
+        from django.contrib.admin.models import LogEntry
+
+        request = context['request']
+
+        tracked = (
+            Photo,
+            Event,
+            Entry,
+            Location,
+            Video,
+        )
+
+        for model in tracked:
+            app_label = model._meta.app_label
+            not_published = '2'
+            if app_label == 'zinnia':
+                not_published = '0'
+
+            self.children.append(
+                {
+                    'title': capfirst(app_label.title())+ ' - ' + model._meta.verbose_name.title(),
+                    'url': reverse('%s:%s_%s_changelist' % (get_admin_site_name(context),app_label,model.__name__.lower())) + '?status__exact='+not_published,
+                    'count': len(model.objects.filter(status__exact=not_published))
+                }
+            )
+
+        if not len(self.children):
+            self.pre_content = _('No pending content.')
+        self._initialized = True
 
 class CustomIndexDashboard(Dashboard):
     """
@@ -70,6 +129,12 @@ class CustomIndexDashboard(Dashboard):
             limit=5,
             collapsible=False,
             column=2,
+        ))
+        
+        self.children.append(NewContent(
+            title=_('User pending content'),
+            column=2,
+            limit=5,
         ))
 
 
