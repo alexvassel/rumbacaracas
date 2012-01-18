445f7bedbd6d421878fdf5a327e382c0241a3305
unknown <Konstantin@Konstantin-PC.(none)>
2012-01-18 18:04:30 +0400

    additional db for localhost

--- host_settings/Konstantin_PC.py
+++ host_settings/Konstantin_PC.py
@@ -46,7 +46,15 @@ DATABASES = {
             'USER': 'rumbaccs', # Not used with sqlite3.
             'HOST': 'ads.2rumba.com', # Set to empty string for localhost. Not used with sqlite3.
             'PASSWORD': 'dXqGg7XHA5Mfax4', # Not used with sqlite3.
-        }
+        },
+    'venezuela': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'venezuela', # Or path to database file if using sqlite3.
+        'USER': 'venezuela', # Not used with sqlite3.
+        'PASSWORD': '_internet01', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    },
 }
 
 DEBUG=True
