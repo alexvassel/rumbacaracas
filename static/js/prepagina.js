6baaf662755f8d5e3c34544ee1d8179d40882bcd
maksim <nagaev.maksim@gmail.com>
2012-04-20 20:06:01 +0400

    Added reffer handling

--- static/js/prepagina.js
+++ static/js/prepagina.js
@@ -1,4 +1,5 @@
 	if($.cookie('prepagina') == null) {
         $.cookie('realreferrer', document.referrer);
+        $.cookie('prepagina', "si");
 		window.location = "http://media.rumbacaracas.com/static/pre.html"
 	}
--- static/js/prepaginave.js
+++ static/js/prepaginave.js
@@ -1,13 +1,5 @@
-
-	function leerCookie(nombre) {
-	   a = document.cookie.substring(document.cookie.indexOf(nombre + '=') + nombre.length + 1,document.cookie.length);
-	   if(a.indexOf(';') != -1)a = a.substring(0,a.indexOf(';'))
-	   return a; 
-	}
-
-	if(leerCookie('prepagina')!="si") {
-		document.cookie = "realreferrer="+encodeURIComponent(document.referrer)+"; path=/";		
-		document.cookie="prepagina = si"
-		document.cookie="direccion = "+location.href
-		window.location = "http://media.rumbacaracas.com/static/preve.html"
-	}
+    if($.cookie('prepagina') == null) {
+       $.cookie('realreferrer', document.referrer);
+       $.cookie('prepagina', "si");
+        window.location = "http://media.rumbacaracas.com/static/preve.html"
+    }
