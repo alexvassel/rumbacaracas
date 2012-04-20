a4ed420c1118f8ed0fd6a6b294f9a49eb7feb23c
maksim <nagaev.maksim@gmail.com>
2012-04-20 20:01:24 +0400

    Added reffer handling

--- static/js/prepagina.js
+++ static/js/prepagina.js
@@ -1,13 +1,4 @@
-
-	function leerCookie(nombre) {
-	   a = document.cookie.substring(document.cookie.indexOf(nombre + '=') + nombre.length + 1,document.cookie.length);
-	   if(a.indexOf(';') != -1)a = a.substring(0,a.indexOf(';'))
-	   return a; 
-	}
-
-	if(leerCookie('prepagina')!="si") {
-		document.cookie = "realreferrer="+encodeURIComponent(document.referrer)+"; path=/";	
-		document.cookie="prepagina = si"
-		document.cookie="direccion = "+location.href
+	if($.cookie('prepagina') == null) {
+        $.cookie('realreferrer', document.referrer);
 		window.location = "http://media.rumbacaracas.com/static/pre.html"
 	}
