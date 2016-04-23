ef38e284c7844e7e03cecb61cb8886fbaa93788b
Aleksandr Vasilev <alexvassel@gmail.com>
2016-04-23 11:21:21 -0400

    #6

--- yourvideos/modelFields.py
+++ yourvideos/modelFields.py
@@ -4,11 +4,12 @@ import re
 class YoutubeField( models.CharField ):
     '''
     transparently check if it a link and parse id
-    '''
-    def get_db_prep_save( self, value, connection=None):
+   '''
+    
+    def get_db_prep_save(self, value, connection):
         if value is not None:
             regex = re.compile( r"^(http://)?(www\.)?(youtube\.com/watch\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})" )
             match = regex.match( value )
             if match:
                 value = match.group( 'id' )
-        return models.CharField.get_db_prep_save( self, value, connection)
+        return value
